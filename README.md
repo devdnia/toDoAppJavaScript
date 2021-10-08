70fe09c95de13c673e4032d02ccfe2b7c1373a6c
devdnia <devdnia@gmail.com>
2021-10-08 11:51:33 +0200

    Primer commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Webpack Starter
+
+Este es el proyecto inicial para crear aplicaciones utilizando webpack.
+
+### Notas:
+Recuerda construir los módulos de Node
+
+```
+npm install
+```
+y construir el build para crear el directorio /dist:
+```
+npm run build
+```
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './styles.css';
+
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+export const todoList = new TodoList();
+
+// Insercción de TODO:
+// const tarea = new Todo( 'Aprender JavaScript!' );
+// todoList.nuevoTodo( tarea );
+// crearTodoHtml( tarea );
+
+todoList.todos.forEach( crearTodoHtml );
+
+console.log( 'todos: ', todoList.todos );
